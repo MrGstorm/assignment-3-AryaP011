47cfa15b629f44557afb92630442c4373d423be1
AryaP011 <APatel7763@hwdsb.on.ca>
2025-10-02 08:38:50 -0400

    Add files via upload
    I removed the Print statements and I moved the comment line to the top!

--- ex7.py
+++ ex7.py
@@ -4,11 +4,11 @@
 #Task: write a program that asks for two numbers and prints the larger one. include a comment at the top describing what the program does
 #########################################################################################################################################
 
+#this code runs by putting in 2 numbers and printing out the big one and by using if and else statements to print the bigger number first. 
 
 numOne = int(input("Please type in your first number: "))
-#print(numOne)
 numTwo=int(input("please type in your second number:"))
-#print(numTwo)
+
 if numOne >  numTwo:
     print(numOne)
 else:
