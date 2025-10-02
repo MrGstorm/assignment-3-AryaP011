42fa39ffb4769bd6cacb3c48d0c172c1d4a9497d
AryaP011 <APatel7763@hwdsb.on.ca>
2025-10-02 08:32:25 -0400

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,18 @@
+########################################################################################################################################
+#Program Name:ex7.py
+#Author: Arya Patel
+#Task: write a program that asks for two numbers and prints the larger one. include a comment at the top describing what the program does
+#########################################################################################################################################
+
+
+numOne = int(input("Please type in your first number: "))
+#print(numOne)
+numTwo=int(input("please type in your second number:"))
+#print(numTwo)
+if numOne >  numTwo:
+    print(numOne)
+else:
+    print(numTwo)
+    
+
+#this code runs by putting in 2 numbers and printing out the big one and by using if and else statements to print the bigger number first. 
