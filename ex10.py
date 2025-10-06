f4d785d6429922ddc7b8ca8e3ecd5403c5ed17bf
AryaP011 <APatel7763@hwdsb.on.ca>
2025-10-06 08:51:57 -0400

    Update ex10.py
    i added a comment line

--- ex10.py
+++ ex10.py
@@ -2,7 +2,7 @@
 #Program Name: ex10.py
 #Author: Arya Patel
 #Task: write a program that asks for 3 friends' name, stores them in a list, and prints them all.
-
+#for this code to work, i used words such as range and append.
 
 friends= []
 for i in range (3):
@@ -15,3 +15,4 @@ for i in range (3):
         print(friend)
 
         
+
