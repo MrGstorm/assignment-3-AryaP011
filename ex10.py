f30de14cd0216e35a30958f86a50ff18a1658faf
AryaP011 <APatel7763@hwdsb.on.ca>
2025-10-06 08:49:50 -0400

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,17 @@
+####################################################
+#Program Name: ex10.py
+#Author: Arya Patel
+#Task: write a program that asks for 3 friends' name, stores them in a list, and prints them all.
+
+
+friends= []
+for i in range (3):
+    name = input(f"enter first friend name #{i+1}:")
+    friends.append(name)
+
+#Print the list of friends
+    print("friend name here:")
+    for friend in friends:
+        print(friend)
+
+        
