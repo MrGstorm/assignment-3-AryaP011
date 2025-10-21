24f3f9834588335a341a1f856dbc50d4e27e70f1
AryaP011 <APatel7763@hwdsb.on.ca>
2025-10-21 08:47:01 -0400

    Add files via upload

---
+++ ex9.py.py
@@ -0,0 +1,12 @@
+#################################################################
+#project name: ex9.py
+# Author: Arya Patel
+# task: write a program that prints all odd numbers from 1 to 19.
+################################################################
+
+
+# for this program to work i used wordas such as range and %
+
+for number in range(1,20):
+    if number %2 !=0:
+        print(number)
